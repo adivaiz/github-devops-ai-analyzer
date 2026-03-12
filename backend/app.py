beffd5d1ccd2f773690f0d35a97b1f463dd5aec7
Adi Vaizman <119796506+adivaiz@users.noreply.github.com>
2026-03-12 14:56:13 +0200

    Update app.py
    connect github listener to app

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from backend.github_listener import router
+
 app = FastAPI()
+
+# חיבור ה-router של GitHub לשרת
+app.include_router(router)
 
 @app.get("/")
 def home():
